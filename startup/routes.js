b9a56cd677c981fc305d32e570511a1a0a014612
Maria Camila Alzate Restrepo <mcamilaar10@gmail.com>
2019-09-22 17:21:24 -0500

    Add cors configuration

--- startup/routes.js
+++ startup/routes.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const games = require('../routes/games');
 const rounds = require('../routes/rounds');
 const error = require('../middleware/error');
 
 module.exports = function (app) {
     app.use(express.json());
+    app.use(cors());
 
     app.use('/api/game', games);
     app.use('/api/round', rounds);
